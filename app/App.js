f58dbe2e2832be76197d12d59e56734cb5c9cdc2
bratelefant <59534030+bratelefant@users.noreply.github.com>
2022-11-29 12:36:23 +0100

    Reconnect in case connection is lost

--- app/App.js
+++ app/App.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import { MainNavigator } from './src/screens/MainNavigator'
-import { View, Text, ActivityIndicator } from 'react-native'
-import { useConnection } from './src/hooks/useConnection'
-import { ErrorMessage } from './src/components/ErrorMessage'
-import { defaultStyles } from './src/styles/defaultStyles'
+import React, { useEffect } from "react";
+import { MainNavigator } from "./src/screens/MainNavigator";
+import { View, Text, ActivityIndicator } from "react-native";
+import { useConnection } from "./src/hooks/useConnection";
+import { ErrorMessage } from "./src/components/ErrorMessage";
+import { defaultStyles } from "./src/styles/defaultStyles";
+import * as SecureStore from "expo-secure-store";
+import config from "./config.json";
+import Meteor from "@meteorrn/core";
 
-export default function App () {
-  const { connected, connectionError } = useConnection()
+export default function App() {
+  const { connected, connectionError } = useConnection();
+
+  useEffect(() => {
+    // connect with Meteor and use a secure store
+    // to persist our received login token, so it's encrypted
+    // and only readable for this very app
+    // read more at: https://docs.expo.dev/versions/latest/sdk/securestore/
+    if (!connected)
+      Meteor.connect(config.backend.url, {
+        AsyncStorage: {
+          getItem: SecureStore.getItemAsync,
+          setItem: SecureStore.setItemAsync,
+          removeItem: SecureStore.deleteItemAsync,
+        },
+      });
+  }, [connected]);
 
   // use splashscreen here, if you like
   if (!connected) {
@@ -15,13 +33,13 @@ export default function App () {
         <ActivityIndicator />
         <Text>Connecting to our servers...</Text>
       </View>
-    )
+    );
   }
 
   // use alert or other things here, if you like
   if (connectionError) {
-    return (<ErrorMessage error={connectionError} />)
+    return <ErrorMessage error={connectionError} />;
   }
 
-  return (<MainNavigator />)
+  return <MainNavigator />;
 }
